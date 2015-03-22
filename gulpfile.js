2a0aea694959b746be1d8bf276f8897429478d3d
Kliment Mamykin <kmamyk@gmail.com>
2015-03-22 13:55:22 -0400

    Added sourcemaps support

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var path = require('path');
 var del = require('del');
 var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('clean', function(callback) {
     del(['./public/**/*'], function (err, deletedFiles) {
@@ -14,15 +15,17 @@ gulp.task('clean', function(callback) {
 
 gulp.task('css', function() {
     return gulp.src('./src/application.less')
+        .pipe(sourcemaps.init())
         .pipe(less({
             paths: [path.join(__dirname, 'node_modules', 'bootstrap', 'less')]
         }))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
-            cascade: false
+            cascade: true
         }))
         .pipe(gulp.dest('./public'))
         .pipe(minifyCSS())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./public/min'));
 });
 
