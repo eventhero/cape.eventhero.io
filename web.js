777b4f445d33c0feda4daaea47e2e7fabb91764a
Kliment Mamykin <kmamyk@gmail.com>
2015-03-24 02:41:42 -0400

    Added caching files for 1 year

--- web.js
+++ web.js
@@ -4,14 +4,16 @@ var compression = require('compression');
 
 // compress all requests
 app.use(compression());
-app.use('/versions', express.static('versions'));
+app.use('/versions', express.static('versions', {
+    maxage: '365 days'
+}));
 
 // TODO: eventually loop through all version and display an index with links
 //app.get('/', function(req, res) {
 //    res.send('Hello World!')
 //});
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     var address = server.address();
     console.log('Example app listening at http://%s:%s', address.address, address.port)
 });
